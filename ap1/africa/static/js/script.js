0e99ef9d4f5982a10f58871f1947c6b350e78fd3
GustaVasconcelos <gustavo.vasconcelos.peixoto18@gmail.com>
2023-03-09 09:08:38 -0300

    inicializando a modelagem do main

--- ap1/africa/static/js/script.js
+++ ap1/africa/static/js/script.js
@@ -12,3 +12,20 @@ buttonMenu.addEventListener('click', () =>{
         buttonMenu.innerHTML = 'close'
     }
 })
+
+let count = 1
+document.getElementById('radio1').checked = true
+
+setInterval(() =>{
+    nextImage()
+},3500)
+
+
+const nextImage = () =>{
+    count++
+    if(count>4){
+        count = 1
+    }
+
+    document.getElementById('radio'+count).checked = true
+}
